292c25faac5fead52c16aa66df35893528c750b3
SkyLing <70288759+SkyLingRQ@users.noreply.github.com>
2023-02-16 19:22:58 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+Instalación:
+
+python -m pip install -r requirements.txt
+
+git clone https://github.com/SkyLingRQ/WrDen
+cd WrDen
+sudo python3 WrDen.py
+
+
+Sistemas Operativos Disponibles:
+
+Kali Linux
+Parrot Security OS
+
+Módulos:
+
+phone_dox     :   Datos de un número telefónico. 
+
+sistem_ip    :   Identificar sistema operativo por la dirección ip.
+
+ip_track        :   Datos de una dirección ip pública.
+
+scan_port     :   Encontrar los puertos abiertos de un host
+
+sms_anom      :   Enviar SMS anonimos a cualquier pais
+
+OPCIONES: 
+
+help   :  Mostrar menú de opciones. 
+
+clear  :  Limpiar la pantalla
+
+
+
---
+++ lib/osint/phone_track.py
@@ -0,0 +1,74 @@
+import requests
+from time import sleep
+import json
+import colorama
+from colorama import Fore,init
+
+verde = Fore.GREEN
+cyan = Fore.CYAN
+rojo = Fore.RED
+magenta = Fore.MAGENTA
+lmagenta = Fore.LIGHTMAGENTA_EX
+blanco = Fore.WHITE
+reset = Fore.RESET
+yellow = Fore.YELLOW
+blue = Fore.BLUE
+
+sleep(1)
+
+banner="""
+·▄▄▄▄        ▐▄• ▄  ▄▄▄· ▄ .▄       ▐ ▄ ▄▄▄ .
+██▪ ██ ▪      █▌█▌▪▐█ ▄███▪▐█▪     •█▌▐█▀▄.▀·
+▐█· ▐█▌ ▄█▀▄  ·██·  ██▀·██▀▐█ ▄█▀▄ ▐█▐▐▌▐▀▀▪▄
+██. ██ ▐█▌.▐▌▪▐█·█▌▐█▪·•██▌▐▀▐█▌.▐▌██▐█▌▐█▄▄▌
+▀▀▀▀▀•  ▀█▄▀▪•▀▀ ▀▀.▀   ▀▀▀ · ▀█▄▀▪▀▀ █▪ ▀▀▀ 
+"""
+
+print (verde+banner)
+
+print (rojo+"INGRESE EL NÚMERO (EJ:+5491188888888):")
+
+try:
+       numero=int(input("---> "+reset))
+except ValueError:
+       print ("porfavor ingrese solamente numeros.")
+
+print (magenta+"sacando información de el número...")
+
+API_KEY='71c9a91b73291f84764eda1c5ccba175'
+
+try:
+       url = 'http://apilayer.net/api/validate?access_key=%s&number=%s&country_code&format=1' % (API_KEY, numero)
+       session=requests.Session()
+       headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+       'Accept':'text/html,application/xhtml+xml,application/xml;'
+       'q=0.9,image/webp,*/*;q=0.8'}
+       req = session.get(url, headers=headers)
+       response=req.json()
+       
+       valid=response['valid']
+       number=response['number']
+       formato_local=response['local_format']
+       formato_internacional=response['international_format']
+       prefijo_origen=response['country_prefix']
+       prefijo_codigo=response['country_code']
+       nombre_origen=response['country_name']
+       localizacion=response['location']
+       empresa=response['carrier']
+       tipo_linea=response['line_type']
+
+       print (blue+"***************************************************************")
+       print (cyan+f"valido: {valid}")
+       print (cyan+f"numero: {number}")
+       print (cyan+f"formato local: {formato_local}")
+       print (cyan+f"formato internacional: {formato_internacional}")
+       print (cyan+f"prefijo: {prefijo_origen}")
+       print (cyan+f"codigo de origen: {prefijo_codigo}")
+       print (cyan+f"nombre del pais: {nombre_origen}")
+       print (cyan+f"nombre de la provincia: {localizacion}")
+       print (cyan+f"empresa: {empresa}")
+       print (cyan+f"tipo de linea: {tipo_linea}")
+       print (blue+"***************************************************************")
+except NameError:
+       print ("el numero que ingreso es incorrecto, porfavor vuelva a intentarlo")
