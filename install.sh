81bc071958bb2ddc346fb5d5feda1cc04bd43f1b
SkyLing <70288759+SkyLingRQ@users.noreply.github.com>
2025-01-02 18:21:19 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-setterm -foreground green
-echo "su instalacion esta comenzando..."
-sleep 1
-
-apt-get install nmap
-apt-get install python
-apt-get install python3
-pip install slowloris
-apt-get install perl
